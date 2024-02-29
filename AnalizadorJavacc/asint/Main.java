557899cdba2b2e3adf6a7bc5e05ba1230e820a2d
serco712 <98959255+serco712@users.noreply.github.com>
2024-02-29 19:48:48 +0100

    Cambios javacc

--- AnalizadorJavacc/asint/Main.java
+++
@@ -1,9 +0,0 @@
-package asint;
-import java.io.FileReader;
-public class Main{
-   public static void main(String[] args) throws Exception {
-      AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(new FileReader(args[0]));
-      asint.disable_tracing();
-      asint.analiza();
-   }
-}
